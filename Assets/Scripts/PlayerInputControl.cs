37ba478a71f7b80c7ac62c357061a66cf84f1b4a
sdfarrar <vague315@gmail.com>
2018-10-22 13:49:20 -0500

    Adds player animations

---
+++ Assets/Scripts/PlayerInputControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TopdownController))]
+public class PlayerInputControl : MonoBehaviour {
+
+    public float MoveSpeed = 40f;
+
+    private TopdownController character;
+    private float moveX;
+    private float moveY;
+
+
+    private void Awake() {
+        character = GetComponent<TopdownController>();
+    }
+	
+	private void Update() {
+        moveX = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+        moveY = Input.GetAxisRaw("Vertical") * MoveSpeed;
+	}
+
+    private void FixedUpdate() {
+        Vector2 movement = new Vector2(moveX, moveY) * Time.fixedDeltaTime;
+        character.Move(movement);
+    }
+
+}
---
+++ Assets/Scripts/TopdownController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopdownController : MonoBehaviour {
+
+    public Animator Animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Vector2 movement) {
+        Debug.Log(movement.x + ", " + movement.y);
+        Animator.SetFloat("MoveX", movement.x);
+        Animator.SetFloat("MoveY", movement.y);
+        Animator.SetBool("IsMoving", movement!=Vector2.zero);
+
+        Vector2 normalizedMove = movement.normalized;
+    }
+}
