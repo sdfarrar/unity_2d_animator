db9cee60b21d47274910d6178d1ae613babb4d1a
vague315 <vague315@gmail.com>
2018-10-22 23:34:04 -0500

    Adds blend tree unity animations.

--- Assets/Scripts/TopdownController.cs
+++ Assets/Scripts/TopdownController.cs
@@ -6,22 +6,56 @@ public class TopdownController : MonoBehaviour {
 
     public Animator Animator;
 
+    private Direction lastDirection;
+
 	// Use this for initialization
 	void Start () {
+		Debug.Log(Animator.runtimeAnimatorController.name);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
     public void Move(Vector2 movement) {
-        Debug.Log(movement.x + ", " + movement.y);
+        string animControllerName = Animator.runtimeAnimatorController.name;
+        //Debug.Log(movement.x + ", " + movement.y);
+
+        if(animControllerName=="PlayerController"){
+            AnimationV1(movement);
+        }else if(animControllerName=="PlayerControllerV2"){
+            AnimationV2(movement);
+        }else{
+            Debug.LogWarning("Unknown Animation Controller: " + animControllerName);
+        }
+
+        Vector2 normalizedMove = movement.normalized;
+    }
+
+    private void AnimationV1(Vector2 movement){
         Animator.SetFloat("MoveX", movement.x);
         Animator.SetFloat("MoveY", movement.y);
         Animator.SetBool("IsMoving", movement!=Vector2.zero);
+    }
 
-        Vector2 normalizedMove = movement.normalized;
+    private void AnimationV2(Vector2 movement){
+        Animator.SetFloat("FaceX", movement.x);
+        Animator.SetFloat("FaceY", movement.y);
+
+        if(movement==Vector2.zero){
+            Animator.Play("Idle");
+        }else{
+            Animator.Play("Walk");
+        }
+    }
+
+    private Direction GetDirectionFromMovement(Vector2 movement) {
+        //TODO make direction from movement to set FaceX and FaceY
+        return Direction.Down;
+    }
+
+    public enum Direction {
+        Up,Down,Left,Right
     }
 }
