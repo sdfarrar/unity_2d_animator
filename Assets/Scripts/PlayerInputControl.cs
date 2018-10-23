f9f3c36c1d23e9cefe09815b97c1d44cb72cb768
sdfarrar <vague315@gmail.com>
2018-10-23 13:04:42 -0500

    Fixes walk up animation. Animates in proper direction based off input

--- Assets/Scripts/PlayerInputControl.cs
+++ Assets/Scripts/PlayerInputControl.cs
@@ -11,6 +11,7 @@ public class PlayerInputControl : MonoBehaviour {
     private float moveX;
     private float moveY;
 
+    private InputState inputState = new InputState();
 
     private void Awake() {
         character = GetComponent<TopdownController>();
@@ -19,11 +20,38 @@ public class PlayerInputControl : MonoBehaviour {
 	private void Update() {
         moveX = Input.GetAxisRaw("Horizontal") * MoveSpeed;
         moveY = Input.GetAxisRaw("Vertical") * MoveSpeed;
+        inputState.Update();
 	}
 
     private void FixedUpdate() {
         Vector2 movement = new Vector2(moveX, moveY) * Time.fixedDeltaTime;
-        character.Move(movement);
+        character.Move(movement, inputState);
+    }
+
+    public struct InputState {
+        public KeyState W,A,S,D;
+        public KeyState Up,Down,Left,Right;
+        public float xAxis;
+        public float yAxis;
+
+        public void Update() {
+            xAxis = Input.GetAxisRaw("Horizontal");
+            yAxis = Input.GetAxisRaw("Vertical");
+            Up = W = new KeyState(KeyCode.W);
+            Left = A = new KeyState(KeyCode.A);
+            Down = S = new KeyState(KeyCode.S);
+            Right = D = new KeyState(KeyCode.D);
+        }
+    }
+
+    public struct KeyState {
+        public bool Down;
+        public bool Held;
+
+        public KeyState(KeyCode code){
+            Down = Input.GetKeyDown(code);
+            Held = Input.GetKey(code);
+        }
     }
 
 }
--- Assets/Scripts/TopdownController.cs
+++ Assets/Scripts/TopdownController.cs
@@ -6,7 +6,8 @@ public class TopdownController : MonoBehaviour {
 
     public Animator Animator;
 
-    private Direction lastDirection;
+    //private Direction lastDirection;
+    private Vector2 lastDirection = Vector2.down;
 
 	// Use this for initialization
 	void Start () {
@@ -18,19 +19,20 @@ public class TopdownController : MonoBehaviour {
 	void Update () {
 	}
 
-    public void Move(Vector2 movement) {
+    public void Move(Vector2 movement, PlayerInputControl.InputState input) {
         string animControllerName = Animator.runtimeAnimatorController.name;
-        //Debug.Log(movement.x + ", " + movement.y);
 
+        Vector2 currentDirection = GetDirectionFromMovement(input);
         if(animControllerName=="PlayerController"){
             AnimationV1(movement);
         }else if(animControllerName=="PlayerControllerV2"){
-            AnimationV2(movement);
+            AnimationV2(movement, currentDirection);
         }else{
             Debug.LogWarning("Unknown Animation Controller: " + animControllerName);
         }
 
         Vector2 normalizedMove = movement.normalized;
+        lastDirection = currentDirection;
     }
 
     private void AnimationV1(Vector2 movement){
@@ -39,9 +41,9 @@ public class TopdownController : MonoBehaviour {
         Animator.SetBool("IsMoving", movement!=Vector2.zero);
     }
 
-    private void AnimationV2(Vector2 movement){
-        Animator.SetFloat("FaceX", movement.x);
-        Animator.SetFloat("FaceY", movement.y);
+    private void AnimationV2(Vector2 movement, Vector2 direction){
+        Animator.SetFloat("FaceX", direction.x);
+        Animator.SetFloat("FaceY", direction.y);
 
         if(movement==Vector2.zero){
             Animator.Play("Idle");
@@ -50,9 +52,19 @@ public class TopdownController : MonoBehaviour {
         }
     }
 
-    private Direction GetDirectionFromMovement(Vector2 movement) {
-        //TODO make direction from movement to set FaceX and FaceY
-        return Direction.Down;
+    private Vector2 GetDirectionFromMovement(PlayerInputControl.InputState input) {
+        // Only return a new direction if it's the only direction being held
+        if(input.Up.Held && !(input.Right.Held || input.Left.Held || input.Down.Held)){
+            return Vector2.up;
+        }else if(input.Down.Held && !(input.Right.Held || input.Left.Held || input.Up.Held)){
+            return Vector2.down;
+        }else if(input.Left.Held && !(input.Down.Held || input.Up.Held || input.Right.Held)){
+            return Vector2.left;
+        }else if(input.Right.Held && !(input.Down.Held || input.Up.Held || input.Left.Held)){
+            return Vector2.right;
+        }
+
+        return lastDirection;
     }
 
     public enum Direction {
