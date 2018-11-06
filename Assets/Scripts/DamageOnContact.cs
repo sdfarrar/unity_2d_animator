1f6b4927d01bc4afb441f222f85a0f05b9fe0420
sdfarrar <vague315@gmail.com>
2018-11-06 13:14:26 -0600

    Adds DamageOnContact script

---
+++ Assets/Scripts/DamageOnContact.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnContact : MonoBehaviour {
+
+    public BoxCollider2D DamageArea;
+    public LayerMask DamageMask;
+
+    private void Update() {
+        Collider2D hit = Physics2D.OverlapBox(Vector2.one * DamageArea.transform.position + DamageArea.offset, DamageArea.size, DamageArea.transform.rotation.eulerAngles.z, DamageMask);
+        if(!hit){ return; }
+
+        Debug.Log("hit " + hit.gameObject.name);
+    }
+
+}
